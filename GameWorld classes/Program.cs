5c8f0eb7f3b6545ce5cb1fcb6e737eb020fc87b2
x3vit <x3vit@hotmail.com>
2022-12-29 13:02:10 +0300

    cleaning programm

--- GameWorld classes/Program.cs
+++ GameWorld classes/Program.cs
@@ -2,23 +2,24 @@
 using GameWorld_classes;
 //4th branch
 Weapon AWP = new Weapon() { Name = "AWP", Ammo = 5, Dmg = 100, Price = 5000 };
-CTWeapon M16 = new CTWeapon() { Name = "M16", Ammo = 30, Dmg = 25, Price = 3000 };
 
+CTWeapon M16 = new CTWeapon() { Name = "M16", Ammo = 30, Dmg = 25, Price = 3000 };
 TWeapon AK47 = new TWeapon() { Name = "AK47", Ammo = 30, Dmg = 30, Price = 3500 };
 
 Player player1 = new Player();
 Player player2 = new Player();
 Player player3 = new Player();
 Player player4 = new Player();
+
 player1.Nickname = "Chubrik1";
 player2.Nickname = "Chubrick2";
 player3.Nickname = "Chubrick3";
 player4.Nickname = "Chubrik4";
+
 player1.Weapon = AWP;
 player2.Weapon = M16;
 player3.Weapon = AK47;
 
-//Player player5 = new Player();
 player1.Position.X = 1;
 player1.Position.Y = 1;
 player2.Position.X = 1;
@@ -27,8 +28,7 @@ player3.Position.X = 4;
 player3.Position.Y = 1;
 player4.Position.X = 4;
 player4.Position.Y = 4;
-//player5.Position.X = 4;
-//player5.Position.Y = 4;
+
 GameWorld server = new GameWorld();
 server.Actions.Add(new PlayerAction { player = player1, action = ActionType.MoveForward });
 
@@ -38,7 +38,7 @@ Armor player1Armor = new Armor();
 Console.WriteLine(player1Armor.ArmorValue);
 Console.WriteLine($"игровое поле {((int)WorldHorizontalFieldX.coordinateX9)} по горизонтали на {((int)WorldVerticalFieldY.coordinateY9)} по вертикали ");
 Weapon players1Weapon = new Weapon();
-//PlayerPosition playerPosition = new PlayerPosition(1,1,270);
+
 
 
 
@@ -66,24 +66,5 @@ player3.Shoot(player3);
 player4.Shoot(player4);
 
 
-//player2.Shoot(player2);
-
-
-//player2.TurnLeft();
-//player2.MoveForward();
-//player2.TurnRight();
-//player1.TurnLeft();
-//player2.TurnLeft();
-//player2.TurnLeft();    
-
-//server.Shoot(player1);server.Shoot(player1);server.Shoot(player1);server.Shoot(player1);server.Shoot(player1);server.Shoot(player1);server.Shoot(player1);server.Shoot(player1);server.Shoot(player1);
-
-
-
-
-
 mapView.PaintMap();
-//server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon); server.Shoot(player2.Position, player2.Weapon);
 
-
-//mapView.PaintStats(player1);
